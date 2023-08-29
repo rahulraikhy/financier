bbf1b547e6d021a80774a1536899e73d66cae329
rahulraikhy <rlraikhy@gmail.com>
2023-08-29 10:44:18 +0100

    Form input option fix

--- src/components/Forms/Form.jsx
+++ src/components/Forms/Form.jsx
@@ -17,10 +17,10 @@ export default function Form() {
 
             <form id="form" onSubmit={handleSubmit(onSubmit)}>
                 <div className="grid gap-4">
-                    <div className="mt-1 block w-full py-2 px-3 border border-gray-200 bg-white rounded-md focus:outline-none focus:ring-indigo-500 sm:text-sm" {...register('type')}>
+                    <div className="input-group">
                         <input type="text" {...register('name')} placeholder="Salary, House Rent" className="form-input" />
                     </div>
-                    <select className="form-input">
+                    <select className="form-input" {...register('type')}>
                         <option value="Investment" defaultValue>Investment</option>
                         <option value="Expense">Expense</option>
                         <option value="Savings">Savings</option>
