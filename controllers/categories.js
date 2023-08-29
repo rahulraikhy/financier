47682e7c7aac66408b9f380681e6eaf5c20eb377
rahulraikhy <rlraikhy@gmail.com>
2023-08-29 16:39:50 +0100

    Controllers finished

--- controllers/categories.js
+++ controllers/categories.js
@@ -20,7 +20,61 @@ async function get_Categories(req, res) {
     return res.json(filter);
 }
 
+async function create_Transaction(req, res) {
+    if (!req.body) return res.status(400).json("Post HTTP Data not Provided");
+    let { name, type, amount } = req.body;
+
+    const create = await new model.create_Transaction(
+        {
+            name,
+            type,
+            amount,
+            date: new Date()
+        }
+    );
+
+    create.save(function (err) {
+        if (!err) return res.json(create);
+        return res.status(400).json({ message: `Error while creating transaction $(err)` });
+    });
+}
+
+async function get_Transaction(req, res) {
+    let data = await model.Transaction.find({});
+    return res.json(data);
+}
+
+async function delete_Transaction(req, res) {
+    if (!req.body) return res.status(400).json({ message: "Request body not Found" });
+    await model.Transaction.deleteOne(req.body, function (err) {
+        if (!err) res.json("Record Deleted!");
+    }).clone().catch(function (err) { res.json("Error while deleting Transaction Record") });
+}
+
+async function get_Labels(req, res) {
+    model.Transaction.aggregate([
+        {
+            $lookup: {
+                from: "categories",
+                localField: "type",
+                foreignField: "type",
+                as: "categories_info"
+            }
+        },
+        {
+            $unwind: "$categories_info"
+        }
+    ]).then(result => {
+        res.json(data);
+    }).catch(error =>
+        res.status(400).json("Look up Collection Error"));
+}
+
 module.exports = {
     create_Categories,
-    get_Categories
+    get_Categories,
+    create_Transaction,
+    get_Transaction,
+    delete_Transaction,
+    get_Labels
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const usersCtrl = require('../../controllers/api/users');
 const ensureLoggedIn = require('../../config/ensureLoggedIn');
 const categoriesCtrl = require('../../controllers/categories');
+const transactionCtrl = require('../../controllers/categories');
+const labelCtrl = require('../../controllers/categories');
 
 // POST /api/users
 router.post('/', usersCtrl.create);
@@ -12,5 +14,13 @@ router.post('/login', usersCtrl.login);
 router.post('/controllers/categories', categoriesCtrl.create_Categories);
 
 router.get('/controllers/categories', categoriesCtrl.get_Categories);
+
+router.post('/controllers/categories', transactionCtrl.create_Transaction);
+
+router.get('/controllers/categories', transactionCtrl.get_Transaction);
+
+router.delete('/controllers/categories', transactionCtrl.delete_Transaction);
+
+router.get('/controllers/categories', labelCtrl.get_Labels);
 
 module.exports = router;
