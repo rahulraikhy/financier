7157d9483ae96a1421e6cfd7f0349f28bd358c3b
rahulraikhy <rlraikhy@gmail.com>
2023-08-29 17:51:33 +0100

    Install Redux Toolkit + store

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './pages/App/App';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import { store } from './store/store';
+import { Provider } from 'react-redux';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>
+  </Provider>
 );
